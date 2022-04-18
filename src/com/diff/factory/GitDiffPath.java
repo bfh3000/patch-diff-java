9fbb1516dcfd73130e0923e1a4ade2f6ab08eb3b
bfh3000 <bfh3000@naver.com>
2022-04-18 09:30:30 +0900

    Refactoring for v1.0.2 (1)
    - gitinore 확인하여 예외 파일 추가되도록.

--- src/com/diff/factory/GitDiffPath.java
+++ src/com/diff/factory/GitDiffPath.java
@@ -3,13 +3,17 @@ package com.diff.factory;
 public final class GitDiffPath {
 	
 	// Commit
-	public static final String INIT_COMMIT_ID = "acb449624b03f6b1280d2731964f9666f6ef83b0"; /* CommitId (Old) */
-	public static final String END_COMMIT_ID = "9c006e4b9048b54d279ca03ee0ccc7ad662c3daf"; /* CommitId (current) */
+	public static final String INIT_COMMIT_ID = "a630528bfd3f9af676df39c3f4dcde593eb71ade"; /* CommitId (Old) */
+	public static final String END_COMMIT_ID = "c4f80b8bb517e011d46d6af1e17d2102d5bf820e"; /* CommitId (current) */
 	// command
 	public static final String COMMAND_GIT_DIFF = "git diff --name-only " + INIT_COMMIT_ID + " " + END_COMMIT_ID;
 	
-	// 원본 소스 위치 (.git)
-	public static final String HOME_PATH = "E:\\003-Git\\001-SAC330\\web\\";
+	// 원본 소스 위치 (.git) 항상 뒤에 \\가 붙어야 함.
+	public static final String HOME_PATH = "E:\\003-Git\\001-SAC330\\proxy\\";
 	// 복사될 최종 위치 (.class)
-	public static final String DESKTOP_PATH = "C:\\Users\\dsm3000\\Desktop\\sac\\";
+	public static final String DESKTOP_PATH = "C:\\Users\\dsm3000\\Desktop\\patchSAC\\";
+	
+	
+	//git ignore 읽어서 처리하도록.
+	public static final String GIT_IGNORE = "";
 }
