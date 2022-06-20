e3894853ac840aa3f944b70ffd0178d2e1123d12
bfh3000 <bfh3000@naver.com>
2022-06-20 15:31:14 +0900

    GitDiffPath 수정

--- src/com/diff/factory/GitDiffPath.java
+++ src/com/diff/factory/GitDiffPath.java
@@ -16,21 +16,22 @@ public final class GitDiffPath {
 		log.debug("GIT_IGNORE : "+GIT_IGNORE);
 	}
 	
-	// Commit
-	public static final String INIT_COMMIT_ID = "4ecf647b3811e6ff71a6ba56d09ba622e59398c7    "; /* CommitId (Old) */
-	public static final String END_COMMIT_ID = "a0112d803644f00e1641146fcfb34d303181c67e    "; /* CommitId (current) */
-	// command
-	public static final String COMMAND_GIT_DIFF = "git diff --name-only " + INIT_COMMIT_ID + " " + END_COMMIT_ID;
+	public static final String GIT_IGNORE = ""; //git ignore 읽어서 처리하도록.
 	
+	/* input!! CommitId (Old) */
+	public static final String INIT_COMMIT_ID = "7ba10548a3b02d997d823258a797286f975cb290 ";
 	
-	//Web은 src 위치가 다름.
-	//option : 1(web), 2(proxy, scheduler)	
-	public static final String PROJECT_CATEGORY = "2";
-	// 원본 소스 위치 ('.git'가 있는 폴더 경로) 
-	public static final String HOME_PATH = "E:\\003-Git\\011-SAC320_KPO_v2\\proxy" 							+"\\";
-	// 복사될 최종 위치 (.class) 
-	public static final String DESKTOP_PATH = "C:\\Users\\dsm3000\\Desktop\\patchSAC-dr" 								+"\\";
+	/* input!! CommitId (current) */
+	public static final String END_COMMIT_ID = "82fee1d6f241d66139386a1f8a35815f4eeab70b "; 
 	
-	//git ignore 읽어서 처리하도록.
-	public static final String GIT_IGNORE = "";
+	/* input!! option : 1(web), 2(proxy, scheduler)*/	
+	public static final String PROJECT_CATEGORY = "1";
+	 
+	/* input!! 원본 소스 위치 ('.git'가 있는 폴더 경로)*/
+	public static final String HOME_PATH = "" 							+"\\";
+	
+	/* input!! 복사될 최종 위치 (.class)*/
+	public static final String DESKTOP_PATH = "" 								+"\\";
+	
+	public static final String COMMAND_GIT_DIFF = "git diff --name-only " + INIT_COMMIT_ID + " " + END_COMMIT_ID; // command	
 }
